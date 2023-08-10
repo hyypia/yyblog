f9cb1272546f59e070371452321d5a4eb47a25f2
igor <igortereschuk92@gmail.com>
2023-08-10 19:12:18 +0200

    created comments template

--- yyblog/blog/views.py
+++ yyblog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
 
@@ -29,12 +29,19 @@ def post_detail(request, post, year, month, day):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
 
 
 @require_POST
 def post_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comments = post.comments.filter(active=True)
     comment = None
     form = CommentForm(data=request.POST)
 
@@ -42,9 +49,10 @@ def post_comment(request, post_id):
         comment = form.save(commit=False)
         comment.post = post
         comment.save()
+        form.clean()
 
     return render(
         request,
-        "blog/post/comment.html",
-        {"post": post, "form": form, "comment": comment},
+        "blog/post/detail.html",
+        {"post": post, "form": form, "comment": comment, "comments": comments},
     )
